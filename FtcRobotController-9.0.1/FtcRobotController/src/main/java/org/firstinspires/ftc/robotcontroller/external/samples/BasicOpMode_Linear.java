3bc7dec9d0b18e0f69bd12666819e8329ae97e0e
Lsiiiyu <sliu24@lacademy.edu>
2023-12-04 16:40:30 -0500

    Update BasicOpMode_Linear.java

--- FtcRobotController-9.0.1/FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/external/samples/BasicOpMode_Linear.java
+++ FtcRobotController-9.0.1/FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/external/samples/BasicOpMode_Linear.java
@@ -57,6 +57,7 @@ public class BasicOpMode_Linear extends LinearOpMode {
     // Declare OpMode members.
     private ElapsedTime runtime = new ElapsedTime();
     private DcMotor leftDrive = null;
+
     private DcMotor rightDrive = null;
 
     @Override
