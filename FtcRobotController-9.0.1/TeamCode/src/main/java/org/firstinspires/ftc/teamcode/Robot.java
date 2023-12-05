620e34f57fdefaa12f17301f6a463ce322c2b42e
Lsiiiyu <sliu24@lacademy.edu>
2023-12-05 16:41:27 -0500

    create robot config.

--- FtcRobotController-9.0.1/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot.java
+++ FtcRobotController-9.0.1/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot.java
@@ -1,9 +1,64 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.hardware.bosch.BNO055IMU;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
 
 public class Robot {
 
     //Declare Actuators
-    DcMotor frontLeft = null;
+    DcMotor leftDrive = null;
+    DcMotor rightDrive = null;
+    Servo intake = null;
+    BNO055IMU imu = null;
+
+    // declare constant
+    private final double wheelSize = 4.0; //inches
+
+    /* local OpMode members. */
+    HardwareMap hwMap = null; //hardware map
+    private final ElapsedTime period = new ElapsedTime();
+
+    /* Constructor */
+    public Robot() {
+
+    }
+
+    /* Initialize standard Hardware interfaces */
+    public void init(HardwareMap ahwMap) {
+        // Save reference to hardware map
+        hwMap = ahwMap;
+
+        leftDrive = hwMap.get(DcMotor.class, "leftDrive");
+        rightDrive = hwMap.get(DcMotor.class, "rightDrive");
+        intake = hwMap.get(Servo.class, "intake");
+
+
+        imu = hwMap.get(BNO055IMU.class, "imu");
+
+        //Temporary Directions for drive train, change after testing if needed.
+        leftDrive.setDirection(DcMotor.Direction.REVERSE);
+        rightDrive.setDirection(DcMotor.Direction.FORWARD);
+
+
+        // set all motors to zero power
+        stopDriveTrain();
+
+
+        // Set all motors to run with encoders.
+        leftDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        rightDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+    }
+
+    private void stopDriveTrain() {
+        leftDrive.setPower(0);
+        rightDrive.setPower(0);
+    }
+
+    //TELE-OP METHODS
+
 }
